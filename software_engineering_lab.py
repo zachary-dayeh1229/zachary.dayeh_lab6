1625ef6c631e0141c961b2b6ce3d8a0e563442ee
zachary-dayeh1229 <127218945+zachary-dayeh1229@users.noreply.github.com>
2023-03-07 10:02:00 -0500

    Add files via upload
    I created the encoded password function and started the menu display.

---
+++ software_engineering_lab.py
@@ -0,0 +1,43 @@
+"""
+Zachary Dayeh
+Lab 6
+Software Engineering
+"""
+
+def menu():  # menu display.
+
+    print("Menu")
+    print("-" * 13)
+    print("1. Encode")
+    print("2. Decode")
+    print("3. Quit")
+    print()
+
+    menu_option = int(input("Please enter an option: "))
+
+    return menu_option
+
+def encode():  # encodes password.
+
+    user_password = int(input("Please enter your password to encode: "))
+    user_password = str(user_password)  # turns to string to iterate.
+    user_password = [str((int(i) + 3)) for i in user_password]  # makes list with list comprehension from string.
+    encoded_password = "".join(user_password)  # joins list.
+    print("Your password has been encoded and stored!")
+    print()
+
+    return encoded_password  # returns the encoded password.
+
+if __name__ == "__main__":
+
+    display_menu = True  # boolean
+
+    while display_menu == True:  # loops until user quits.
+
+        menu_option = menu()  # calls menu to print and return value.
+
+        if menu_option == 1:
+            encode()  # calls encode function.
+
+        if menu_option == 3:
+            break
